bb94c88741eb1397708b27d6ef7cef58f0254ea9
jake <jacob.r.sanford@gmail.com>
2024-07-09 18:32:41 -0500

    style changes

--- theworkswebsite/src/main/java/com/theworks/controllers/ShopController.java
+++ theworkswebsite/src/main/java/com/theworks/controllers/ShopController.java
@@ -1,12 +1,14 @@
 package com.theworks.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.theworks.entities.Product;
 import com.theworks.repos.ProductRepo;
+
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 import jakarta.servlet.ServletRequest;
-import java.util.List;
-import java.util.Optional;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -17,16 +19,18 @@ import lombok.extern.slf4j.Slf4j;
 @ViewScoped
 @Named
 public class ShopController {
-	
-	private Product			selectedProduct	= null;
-	private List<Product>	productList		= null;
-	
-	public ShopController(	ServletRequest request,
-							ProductRepo productRepo) {
+
+	private Product selectedProduct = null;
+	private List<Product> productList = null;
+
+	public ShopController(ServletRequest request, ProductRepo productRepo) {
+
 		Integer productId = null;
-		try {
-			productId = Integer.parseInt((String) request.getAttribute("productId"));
-		} catch (NumberFormatException e) {
+		if (request.getAttribute("productId") != null) {
+			try {
+				productId = Integer.parseInt((String) request.getAttribute("productId"));
+			} catch (NumberFormatException e) {
+			}
 		}
 		if (productId != null) {
 			Optional<Product> foundProductOptional = productRepo.findById(productId);
@@ -36,7 +40,9 @@ public class ShopController {
 				productList = productRepo.findAllByListed(true);
 			}
 		} else {
+			log.info("***loading clothing items");
 			productList = productRepo.findAllByListed(true);
 		}
+		log.info("***id: " + productId);
 	}
 }
--- theworkswebsite/src/main/java/com/theworks/entities/Product.java
+++ theworkswebsite/src/main/java/com/theworks/entities/Product.java
@@ -1,5 +1,8 @@
 package com.theworks.entities;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,39 +15,39 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
-import java.math.BigDecimal;
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
+// image dimensions should be 3024x4032
+// or 3x4 in scale
 @Table(name = "product", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 @Entity
 @Getter
 @Setter
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int					id;
-	private String				name;
-	private String				description;
-	private BigDecimal			cost;
-	private BigDecimal			price;
-	private String				size;
-	private boolean				listed	= false;
-	private List<String>		types;
-	private List<String>		colors;
-	private List<String>		materials;
+	private int id;
+	private String name;
+	private String description;
+	private BigDecimal cost;
+	private BigDecimal price;
+	private String size;
+	private boolean listed = false;
+	private List<String> types;
+	private List<String> colors;
+	private List<String> materials;
 	@Lob
 	@Column(columnDefinition = "BLOB")
-	private byte[]				frontImage;
+	private byte[] frontImage;
 	@Lob
 	@Column(columnDefinition = "BLOB")
-	private byte[]				backImage;
+	private byte[] backImage;
 	@Transient
-	private byte[]				backImageMock;
+	private byte[] backImageMock;
 	@Transient
-	private byte[]				frontImageMock;
+	private byte[] frontImageMock;
 	@OneToMany(mappedBy = "product", fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
-	private List<OtherImage>	otherImages;
+	private List<OtherImage> otherImages;
 }
